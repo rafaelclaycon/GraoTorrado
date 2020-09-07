7b2335d725d19da72b91af5254362338fa79952e
Rafael Claycon Schmitt <rafael.claycon@gmail.com>
2020-09-07 15:40:33 -0300

    Commit inicial

---
+++ GraoTorrado/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  GraoTorrado
+//
+//  Created by Rafael Schmitt on 03/09/20.
+//  Copyright © 2020 Rafael Schmitt. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GraoTorrado/Modelos/Cafe.swift
@@ -0,0 +1,16 @@
+//
+//  Cafe.swift
+//  GraoTorrado
+//
+//  Created by Rafael Schmitt on 05/09/20.
+//  Copyright © 2020 Rafael Schmitt. All rights reserved.
+//
+
+import UIKit
+
+struct Cafe {
+    var id: Int
+    var nome: String
+    var marca: String
+    var image: UIImage?
+}
